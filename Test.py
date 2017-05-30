cb8e669e9c215559330401a517296e6478e8ca4c
Tobias Laving <tobbeman1994@gmail.com>
2017-05-30 23:09:42 +0200

    Easy start

--- Test.py
+++ Test.py
@@ -1,8 +1,6 @@
 import sys, pygame
 pygame.init()
 
-size = width, height = 320, 240
-
 #Colors
 red = (255,0,0)
 green = (0,255,0)
@@ -12,14 +10,24 @@ white = (255,255,255)
 black = (0,0,0)
 pink = (255,200,200)
 
+#Screen
+size = width, height = 320, 240
 screen = pygame.display.set_mode(size)
 
+#Player
 player = pygame.Rect(0,0,20,20)
-playerSpeed = 2
+dx = 0
+dy = 0
+playerSpeed = [1, 1]  #x,y
+
+#Variables
+gravity = 0.1
 
 #Flags
 moveLeft = False
 moveRight = False
+moveUp = False
+moveDown = False
 
 while 1:
     #Handle events
@@ -32,12 +40,40 @@ while 1:
                 moveLeft = True
             if event.key == pygame.K_RIGHT:
                 moveRight = True
+            if event.key == pygame.K_UP:
+                moveUp = True
+            if event.key == pygame.K_DOWN:
+                moveDown = True
+                
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                moveLeft = False
+                dx = 0
+            if event.key == pygame.K_RIGHT:
+                moveRight = False
+                dx = 0
+            if event.key == pygame.K_UP:
+                moveUp = False
+                dy = 0
+            if event.key == pygame.K_DOWN:
+                moveDown = False
+                dy = 0
 
-    #Logic
+    #Movement Logic
     if moveLeft:
-        player.x -= playerSpeed
+        dx = -playerSpeed[0]
     if moveRight:
-        player.x += playerSpeed
+        dx = playerSpeed[0]
+    if moveUp:
+        dy = -playerSpeed[1]
+    if moveDown:
+        dy = playerSpeed[1]
+
+    #Collision
+    if 0 < player.x + dx and player.x + player.width + dx < width:
+        player.x += dx
+    if 0 < player.y + dy and player.y + player.height + dy < height:
+        player.y += dy
 
     #Print screen
     screen.fill(black)
